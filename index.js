c78e90225fc81004878e96ce4a74c001fbc0a2cc
JosephShin <joespam9999@gmail.com>
2022-04-19 16:17:43 -0400

    commit tree 555a64d009f6bd4309818c98f4c3ac5fac1869ea

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for(i = 0; i < array.length; i++){
+    let targetDiff = target - array[i]
+    let filtered = array.filter(num => num === targetDiff)
+    debugger
+    if(filtered.length > 1){
+      return true
+    }else if(filtered[0] === array[i]){
+      return false
+    }else if(filtered[0] === (targetDiff)){
+      return true
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n)
 */
 
 /* 
   Add your pseudocode here
+
+  //check to see what number is needed to return true. i.e target = 10. 1st value is 3. Filter for 7
+  //if that value exists, return true
+  //if it doesn't, go to next number in array
+
 */
 
 /*
   Add written explanation of your solution here
+
+  //filtering for the difference between target and number in array
+  //if that's true we found it!
+
 */
 
 // You can run `node index.js` to view these console logs
